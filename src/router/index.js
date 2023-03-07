a1f8801e259992faf7e399386dcdf5e29c8630ca
Tony Peavey <tonypeavery@gmail.com>
2023-03-07 12:09:04 -0600

    first deploy

---
+++ src/router/index.js
@@ -0,0 +1,28 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import EventListView from '../views/EventListView.vue'
+import EventDetail from '../views/EventDetails.vue'
+import AboutView from '../views/AboutView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'event-list',
+      component: EventListView
+    },
+    {
+      path: '/event/:id',
+      name: 'event-details',
+      props: true,
+      component: EventDetail
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: AboutView,
+    }
+  ]
+})
+
+export default router
